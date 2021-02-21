456057f2096e5c4e87410dffa841816ede326de2
GitSok <soksinead.okane@outlook.com>
2021-02-21 15:57:31 +0000

    '3rd Commit'

--- Housing Prices.py
+++ Housing Prices.py
@@ -2,9 +2,10 @@
 import pandas as pd
 import numpy as np
 #HP=HousePrice/Second Hand House Price=SecondHP
-NewHP=pd.read_csv(r'C:\Users\soksi\Downloads\form_41a-price-new-property-area-by_year_2.csv')
-SecondHP=pd.read_csv(r'C:\Users\soksi\Downloads\form_41c-price-sh-property-area-by_year_1.csv')
+NewHP=pd.read_csv(r'C:\Users\soksi\OneDrive\Desktop\Annual New Property Price.csv')
+SecondHP=pd.read_csv(r'C:\Users\soksi\OneDrive\Desktop\Second Hand Property prices.csv')
 print(NewHP.head())
-print(NewHP.describe())
+print(SecondHP.describe)
 print(SecondHP.head())
 print(SecondHP.describe())
+
