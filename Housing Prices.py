d3e0571e8e7ea622a7c8cc89989d9bcd8b77bf84
GitSok <soksinead.okane@outlook.com>
2021-02-21 14:45:37 +0000

    'First Commit'

---
+++ Housing Prices.py
@@ -0,0 +1,10 @@
+#Import Pandas as Pd/Import numpy as np
+import pandas as pd
+import numpy as np
+#HP=HousePrice/Second Hand House Price=SecondHP
+NewHP=pd.read_csv(r'C:\Users\soksi\Downloads\form_41a-price-new-property-area-by_year_2.csv')
+SecondHP=pd.read_csv(r'C:\Users\soksi\Downloads\form_41c-price-sh-property-area-by_year_1.csv')
+print(NewHP.head())
+print(NewHP.info())
+print(SecondHP.head())
+print(SecondHP.info())
