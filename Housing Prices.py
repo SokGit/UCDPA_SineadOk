bae9298eb1e21a68ea03c2cd7a368cebcf58b91e
GitSok <soksinead.okane@outlook.com>
2021-02-22 22:36:37 +0000

    'Mean Commit'

--- Housing Prices.py
+++ Housing Prices.py
@@ -8,4 +8,7 @@ print(NewHP.head())
 print(SecondHP.describe)
 print(SecondHP.head())
 print(SecondHP.describe())
-
+CelticTigerYears2006_2008=np.array([305269,322634,305269])
+CrashYears2009_2011=np.array([242033,228268,230303])
+print(CelticTigerYears2006_2008.mean())
+print(CrashYears2009_2011.mean())
